dc17b40375d9c0ed36b5dada2bbfaf8d63472b43
c-p-wutti <christopher.wutti@gmx.at>
2017-10-27 12:15:35 +0200

    Updated UI Meeting

--- Android/Terminverwaltung_app/app/src/main/java/at/ghtw/terminverwaltung_app/MeetingActivity.java
+++ Android/Terminverwaltung_app/app/src/main/java/at/ghtw/terminverwaltung_app/MeetingActivity.java
@@ -2,12 +2,30 @@ package at.ghtw.terminverwaltung_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MeetingActivity extends AppCompatActivity {
+
+    Toolbar tbMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting);
+
+        tbMenu = (Toolbar) findViewById(R.id.tbMenu);
+        setSupportActionBar(tbMenu);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater mMenuInflater = getMenuInflater();
+        mMenuInflater.inflate(R.menu.menu_meeting_activity, menu);
+        return true;
+    }
+
+
+
 }
